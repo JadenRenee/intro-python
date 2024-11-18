89f5c569f6c7216ac2ac86f9866daee4285cfa48
JadenRenee <134346377+JadenRenee@users.noreply.github.com>
2024-11-17 23:45:05 -0500

    Add files via upload

---
+++ Johnson_Jaden_python_intro.py
@@ -0,0 +1,15 @@
+if 3 > 2:
+    print('It works!')
+
+    if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+    name = 'Jaden'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Jaden':
+    print('Hey Jaden!')
+else:
+    print('Hey anonymous!')
